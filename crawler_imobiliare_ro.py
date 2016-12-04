ffcb4d347631dae073de030ef490fbba59fd3aa5
cocos <coco.marinaru@gmail.com>
2016-12-04 19:55:17 +0200

    refactoring

--- crawler_imobiliare_ro.py
+++ crawler_imobiliare_ro.py
@@ -4,34 +4,35 @@ import constants
 
 MAX_PAGES = 68
 
+
+def get_search_result_url(page):
+    return constants.IMOBILIARE_RO['SEARCH_RESULTS_URL'].format(page)
+
+
 def crawl():
     apartments = []
     for page in range(1, MAX_PAGES):
-        apartments.extend(get_apartments_from_search_results(page))
+        search_results_url = get_search_result_url(page)
+        results = get_apartments_from_search_results(search_results_url)
+        apartments.extend(results)
 
     print("Number of apartments:", len(apartments))
-    for ap in apartments:
-        print(ap)
 
 
-def get_apartments_from_search_results(page):
+def get_apartments_from_search_results(url):
     apartments = []
-    url = constants.IMOBILIARE_RO['SEARCH_RESULTS_URL'].format(page)
-    print("Parsing",page, url)
+
+    print("Parsing: ", url)
     response = requests.get(url)
     tree = html.fromstring(response.content)
-    # results = tree.xpath("//div[@id='container-lista-rezultate']")
-    # posts = tree.xpath("//div[@class='box-anunt']//")
     post_links = tree.xpath(constants.IMOBILIARE_RO['XPATH_APARTMENT_ANCHORS'])
 
     for link in post_links:
-        apartment = {}
-        apartment['title'] = link.attrib['title']
-        apartment['link'] = link.attrib['href']
+        apartment = {'title': link.attrib['title'], 'link': link.attrib['href']}
         apartments.append(apartment)
 
-    print("Found ",len(apartments))
     return apartments
 
 
-crawl()
+if __name__ == "__main__":
+    crawl()
